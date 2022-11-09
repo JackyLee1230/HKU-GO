4bf821ea048385368e0bd4ae228369cab2ab9c8c
Lee Chi Ho <u3578552@connect.hku.hk>
2022-11-10 00:48:27 +0800

    fix: add permission prompt

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,37 +1,67 @@
-import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View , Dimensions} from 'react-native';
+import { Camera, CameraType } from "expo-camera";
+import { useState } from "react";
+import {
+	Button,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+	Dimensions,
+} from "react-native";
 
 export default function CameraCompo() {
-  const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
+	const [type, setType] = useState(CameraType.back);
+	const [permission, requestPermission] = Camera.useCameraPermissions();
 
+	function toggleCameraType() {
+		setType((current) =>
+			current === CameraType.back ? CameraType.front : CameraType.back
+		);
+	}
 
-  function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
-  }
-
-  return (
-    <View style={styles.cameraContainer}>
-      <Camera style={styles.fixedRatio} type={type}>
-        <View >
-          <TouchableOpacity onPress={toggleCameraType}>
-            <Text >Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-    </View>
-  );
-  
+	return (
+		<>
+			{!permission ? (
+				<View>
+					<Text>Loading</Text>
+				</View>
+			) : (
+				<>
+					{!permission.granted ? (
+						<View style={styles.container}>
+							<Text style={{ textAlign: "center" }}>
+								Please Grant Permission To Use Your Phone's Camera
+							</Text>
+							<Button onPress={requestPermission} title="grant permission" />
+						</View>
+					) : (
+						<View style={styles.cameraContainer}>
+							<Camera
+								style={styles.fixedRatio}
+								type={type}
+								ratio={"16:9"}
+								whiteBalance={900}
+							>
+								<View>
+									<TouchableOpacity onPress={toggleCameraType}>
+										<Text>Flip Camera</Text>
+									</TouchableOpacity>
+								</View>
+							</Camera>
+						</View>
+					)}
+				</>
+			)}
+		</>
+	);
 }
 const styles = StyleSheet.create({
-    cameraContainer: {
-        flex: 1,
-        flexDirection: 'row'
-    },
-    fixedRatio:{
-        width: Dimensions.get("window").width,
-        height: Dimensions.get("window").height,
-    }
-  }
-)
+	cameraContainer: {
+		flex: 1,
+		flexDirection: "row",
+	},
+	fixedRatio: {
+		width: Dimensions.get("window").width,
+		height: Dimensions.get("window").height,
+	},
+});
