7c80171661ce153030d4e1fea6f15a6db1204884
Lee Chi Ho <u3578552@connect.hku.hk>
2022-11-20 22:26:41 +0800

    fix: styles

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,68 +1,81 @@
-import React from 'react';
-import { Text, Image, View } from 'react-native';
-import { Appbar } from 'react-native-paper';
+import React from "react";
+import { Text, Image, View } from "react-native";
+import { Appbar } from "react-native-paper";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function NavBar(scene) {
-    const { userName, userPoint, userAvatar } = scene.options;
+	const { userName, userPoint, userAvatar } = scene.options;
 
-    return (
-        <Appbar.Header
-            style={{
-                backgroundColor: "#47B5FF",
-                paddingHorizontal: 36,
-            }}
-        >
-            <View
-                stlye={{
-                    justifyContent: 'center',
-                    alignSelf: 'center',
-                    alignItem: 'center',
-                }}
-            >
-                <Text
-                    style={{
-                        fontWeight: "500",
-                        fontSize: 24,
-                        color: "#FFFFFF",
-                    }}
-                >
-                    {userName}
-                </Text>
-            </View>
+	return (
+		<Appbar.Header
+			style={{
+				backgroundColor: "#47B5FF",
+				paddingHorizontal: 36,
+				color: "white",
+				justifyContent: "space-between",
+			}}
+		>
+			<View
+				stlye={{
+					justifyContent: "center",
+					alignSelf: "center",
+					alignItem: "center",
+				}}
+			>
+				<Text
+					style={{
+						fontWeight: "500",
+						fontSize: 24,
+						color: "#FFFFFF",
+					}}
+				>
+					{userName}
+				</Text>
+			</View>
+			<View
+				style={{
+					flex: 1,
+					flexDirection: "row",
+					alignItems: "center",
+				}}
+			>
+				<Icon
+					name={"star"}
+					color={"#FFFFFF"}
+					size={24}
+					style={{ marginLeft: 36 }}
+				/>
+				<Text
+					style={{
+						fontWeight: "500",
+						fontSize: 24,
+						color: "#FFFFFF",
+						marginLeft: 12,
+						marginRight: 36,
+					}}
+				>
+					{userPoint}
+				</Text>
+			</View>
 
-            <Icon name={"star"} color={"#FFFFFF"} size={24} style={{ marginLeft: 36,}} />
-            <Text
-                style={{
-                    fontWeight: "500",
-                    fontSize: 24,
-                    color: "#FFFFFF",
-                    marginLeft: 12,
-                    marginRight: 36,
-                }}
-            >
-                {userPoint}
-            </Text>
-
-            <View
-                stlye={{
-                    justifyContent: 'center',
-                    alignSelf: 'center',
-                    alignItem: 'center',
-                }}
-            >
-                <Image
-                    style={{
-                        height: 48,
-                        width: 48,
-                        borderRadius: 100,
-                    }}
-                    source={{
-                        uri: userAvatar,
-                    }}
-                />
-            </View>
-        </Appbar.Header>
-    )
-};
- 
+			<View
+				stlye={{
+					justifyContent: "center",
+					alignSelf: "center",
+					alignItem: "center",
+				}}
+			>
+				<Image
+					style={{
+						height: 48,
+						width: 48,
+						borderRadius: 100,
+					}}
+					source={{
+						uri: userAvatar,
+					}}
+				/>
+			</View>
+		</Appbar.Header>
+	);
+}
