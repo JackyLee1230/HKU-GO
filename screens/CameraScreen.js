9aa79309514010b68393be8901bbca8b1603fccc
Lee Chi Ho <u3578552@connect.hku.hk>
2022-11-22 14:28:52 +0800

    feat: working ai

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,5 +1,5 @@
 import { Camera, CameraType } from "expo-camera";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
 	StyleSheet,
 	Text,
@@ -10,6 +10,16 @@ import {
 	Modal,
 	ActivityIndicator,
 } from "react-native";
+import {
+	collection,
+	addDoc,
+	query,
+	where,
+	getDocs,
+	deleteDoc,
+	doc,
+	setDoc,
+} from "firebase/firestore";
 import { FAB } from "react-native-paper";
 import { LinearGradient } from "expo-linear-gradient";
 import {
@@ -18,6 +28,8 @@ import {
 	startPrediction,
 } from "../src/tensor-helper";
 import { cropPicture } from "../src/image-helper";
+import { updateDoc } from "firebase/firestore";
+import { auth, db } from "../firebase";
 
 const RESULT_MAPPING = ["Centennial Campus", "Happy Park", "Main Building"];
 
@@ -43,12 +55,76 @@ const CameraCompo = ({ navigation }) => {
 
 		const prediction = await startPrediction(model, tensor);
 
-		const highestPrediction = prediction.indexOf(
-			Math.max.apply(null, prediction)
-		);
-		console.log(highestPrediction);
-		setPresentedShape(RESULT_MAPPING[highestPrediction]);
+		console.log(prediction);
+		let highestPrediction = 0;
+		console.log(Math.max.apply(null, prediction));
+		if (Math.max.apply(null, prediction) > 0) {
+			highestPrediction = prediction.indexOf(Math.max.apply(null, prediction));
+			setPresentedShape(RESULT_MAPPING[highestPrediction]);
+		} else {
+			highestPrediction = -1;
+			setPresentedShape("Not Valid");
+		}
 	};
+
+	const addPoints = async () => {
+		const q = query(
+			collection(db, "points"),
+			where("uid", "==", auth?.currentUser?.uid)
+		);
+		const querySnapshot = await getDocs(q);
+		let result = [];
+		querySnapshot.forEach((doc) => {
+			let temp = doc.data();
+			temp.id = doc.id;
+			result.push(temp);
+		});
+		// get the value of mapHunt array
+		let mapHunt = result[0].mapHunt;
+		console.log(mapHunt);
+
+		let exist = 0;
+
+		// check if the presentedShape is in the mapHunt array
+		if (mapHunt.includes(presentedShape)) {
+			// if yes, do nothing
+			exist = 1;
+		} else {
+			// if no, add the presentedShape to the mapHunt array
+			mapHunt.push(presentedShape);
+			console.log("updated maphunt array: ", mapHunt);
+			// update the mapHunt array in the database
+			updateDoc(doc(db, "points", result[0].id), {
+				mapHunt: mapHunt,
+			});
+		}
+
+		if (exist === 0) {
+			const q3 = query(
+				collection(db, "points"),
+				where("uid", "==", auth?.currentUser?.uid)
+			);
+			const querySnapshot3 = await getDocs(q3);
+			let result3 = [];
+			querySnapshot3.forEach((doc) => {
+				let temp = doc.data();
+				temp.id = doc.id;
+				result3.push(temp);
+			});
+			if (result3.length > 0) {
+				const docRef3 = doc(db, "points", result3[0].id);
+				await updateDoc(docRef3, {
+					points: result3[0].points + 20,
+				});
+			}
+		}
+	};
+
+	// useEffect(() => {
+	// 	if (presentedShape !== "" && presentedShape !== "Not Valid") {
+	// 		addPoints();
+	// 	}
+	// }, [presentedShape]);
 
 	return (
 		<>
@@ -66,13 +142,25 @@ const CameraCompo = ({ navigation }) => {
 						>
 							<View style={styles.modal}>
 								<View style={styles.modalContent}>
-									<Text>Your current shape is {presentedShape}</Text>
-									{presentedShape === "" && <ActivityIndicator size="large" />}
+									{presentedShape !== "Not Valid" && presentedShape !== "" ? (
+										<>
+											<Text>Your visited {presentedShape}</Text>
+											<Text>20 Pointshave been awarded</Text>
+										</>
+									) : null}
+
+									{!RESULT_MAPPING.includes(presentedShape) &&
+										presentedShape !== "" &&
+										presentedShape !== "Not Valid" && (
+											<ActivityIndicator size="large" />
+										)}
 									<Pressable
 										style={styles.dismissButton}
-										onPress={() => {
+										onPress={async () => {
 											setPresentedShape("");
 											setIsProcessing(false);
+											await addPoints();
+											console.log(auth?.currentUser.uid);
 										}}
 									>
 										<Text>Dismiss</Text>
@@ -84,7 +172,8 @@ const CameraCompo = ({ navigation }) => {
 							<>
 								<Button
 									onPress={async () => {
-										let res = await Camera.requestCameraPermissionsAsync();
+										let res =
+											await Camera.requestCameraPermissionsAsync().then();
 										if (res.granted == true) {
 											permission.granted = true;
 										}
