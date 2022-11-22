26f6f2c77ad48a5160f79f6d372c950533c9cbad
Siu Yuk Shing <79687526+Samsys1622@users.noreply.github.com>
2022-11-22 20:41:04 +0800

    fix: mapHunt navigation

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,7 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("@expo/metro-config");
 
-module.exports = (async () => {
-	const {
-		resolver: { assetExts },
-	} = await getDefaultConfig(__dirname);
-	return {
-		transformer: {
-			getTransformOptions: async () => ({
-				transform: {
-					experimentalImportSupport: false,
-					inlineRequires: false,
-				},
-			}),
-		},
-		resolver: {
-			assetExts: [...assetExts, "bin"],
-		},
-	};
-})();
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts.push("cjs");
+
+module.exports = defaultConfig;
--- src/navigation/MapStackNavigator.js
+++ src/navigation/MapStackNavigator.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MapScreen from "../../screens/MapScreen";
+import MapHuntScreen from "../../screens/MapHuntScreen";
 import NavBar from "../NavBar";
 
 const Stack = createNativeStackNavigator();
 
 const MapStackNavigator = () => {
 	return (
-		<Stack.Navigator headerMode="screen" screenOptions={{ headerShown: false }}>
+		<Stack.Navigator headerMode="screen">
 			<Stack.Screen
 				name="Map"
 				component={MapScreen}
 				options={{
 					title: "Map",
+					headerShown: false,
+				}}
+			/>
+			<Stack.Screen
+				name="MapHunt"
+				component={MapHuntScreen}
+				options={{
+					title: "Map Hunt",
+					headerStyle: {
+						backgroundColor: '#47B5FF',
+					},
+					headerTintColor: '#fff',
+					headerTitleStyle: {
+						fontWeight: 'bold',
+					},
 				}}
 			/>
 		</Stack.Navigator>
